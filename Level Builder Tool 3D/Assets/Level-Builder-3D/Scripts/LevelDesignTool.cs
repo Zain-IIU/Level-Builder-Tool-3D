fa4643a2c96e2861d7b89dae24975b22ad8f78a0
Arex360 <49303647+Arex360@users.noreply.github.com>
2022-08-17 18:20:15 +0500

    Improved Client Interaction
    - Added Toggle mode
    - Only Canvas of current mode shown
    - Preview Spawnable
    - Spawn with transition

--- Level Builder Tool 3D/Assets/Level-Builder-3D/Scripts/LevelDesignTool.cs
+++ Level Builder Tool 3D/Assets/Level-Builder-3D/Scripts/LevelDesignTool.cs
@@ -1,14 +1,20 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.EventSystems;
-
+using TMPro;
 
 // ReSharper disable once CheckNamespace
 public class LevelDesignTool : MonoBehaviour
 {
+    public TextMeshProUGUI modeText;
+    public GameObject propWindow;
+    public Material transparentMat;
+    public TileInfo previewModel;
+    public GameObject previewModelObj;
+    public GameObject tileWindow;
     private enum State
     {
         Tiling,
@@ -18,6 +24,7 @@ public class LevelDesignTool : MonoBehaviour
     [SerializeField] private LevelSaveSystem saveSystem;
     [SerializeField] private TileInfo[] groundTilePrefabs;
     [SerializeField] private GameObject[] prop;
+    [SerializeField] private GameObject[] previewProp;
     private readonly List<Transform> _storedTiles = new List<Transform>();
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private LayerMask handlerLayer;
@@ -38,7 +45,9 @@ public class LevelDesignTool : MonoBehaviour
     private GameObject _propInfo;
     #endregion
     
-    
+    private void Awake() {
+        SetUpMode();
+    }
     private void Update()
     {
         if (Camera.main is { }) _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -78,6 +87,7 @@ public class LevelDesignTool : MonoBehaviour
                     _curTilePrefabIndex++;
                     _curTilePrefabIndex %= groundTilePrefabs.Length;
                 }
+                PreviewRaycastHandles();
                 if (Input.GetMouseButtonDown(0))
                 {
                     if (EventSystem.current.IsPointerOverGameObject()) return;       
@@ -115,7 +125,31 @@ public class LevelDesignTool : MonoBehaviour
                   pointer.transform.parent = _storedTiles[_storedTiles.Count - 1];
                   pointer.DOLocalMove(new Vector3(0, 1, 0), .1f);
               }
+                private void PreviewRaycastHandles(){
+                     if (!Physics.Raycast(_ray, out _hit, 200,handlerLayer)) return;
       
+                  var newPos = _hit.transform.GetComponentInParent<TileInfo>().transform.localPosition;
+                  newPos.y = heightAdjuster;
+                  switch (_hit.transform.tag) 
+                  {
+                      case "Forward":
+                          newPos.z += 2;
+                          previewModel.transform.position = newPos;
+                          break;
+                      case "Backward":
+                          newPos.z -= 2;
+                            previewModel.transform.position = newPos;
+                          break;
+                      case "Left":
+                          newPos.x -= 2;
+                          previewModel.transform.position = newPos;
+                           break;
+                      case "Right":
+                          newPos.x += 2;
+                          previewModel.transform.position = newPos;
+                           break;
+                  }
+                }
               private void RaycastHandles()
               {
                   if (!Physics.Raycast(_ray, out _hit, 200,handlerLayer)) return;
@@ -149,14 +183,23 @@ public class LevelDesignTool : MonoBehaviour
                   if (!Physics.Raycast(_ray, out _hit, 200,tileLayer)) return;
 
                   propPointer.transform.position = _hit.point;
+                  if(previewModelObj){
+                    previewModelObj.transform.parent = _hit.collider.transform;
+                     previewModelObj.transform.position = _hit.point;
+                  }
                   
                   if (!_gotProp && Input.GetMouseButton(0))
                   {
                       _gotProp = true;
                       _propInfo=Instantiate(prop[_curPropPrefabIndex], transform, true);
+                      Vector3 scale = _propInfo.transform.localScale;
+                      _propInfo.transform.localScale = Vector3.zero;
+                      _propInfo.transform.DOScale(scale, .3f).SetEase(Ease.OutBounce);
+                      previewModelObj.transform.DOScale(scale,.3f);
                       _storedTiles.Add(_propInfo.GetComponent<Transform>());
                       _propInfo.transform.parent = _hit.collider.transform;
                       _propInfo.transform.position = _hit.point;
+                      //Destroy(previewModelObj);
                   }
 
                   if (Input.GetMouseButtonDown(0))
@@ -181,6 +224,9 @@ public class LevelDesignTool : MonoBehaviour
               private void SpawnNewElementAt(Vector3 newPos)
               {
                   var levelElement = Instantiate(groundTilePrefabs[_curTilePrefabIndex], transform, true);
+                  Vector3 scale = levelElement.transform.localScale;
+                  levelElement.transform.localScale = Vector3.zero;
+                  levelElement.transform.DOScale(scale, .2f);
                   print("new element added");
                   var transform2 = levelElement.transform;
                   transform2.localPosition = newPos;
@@ -193,11 +239,39 @@ public class LevelDesignTool : MonoBehaviour
 
     #endregion
     
-    public void SetTileIndex(int value) => _curTilePrefabIndex = value;
+    public void SetTileIndex(int value) {
+         _curTilePrefabIndex = value;
+         if(previewModel){
+            Destroy(previewModel.gameObject);
+         }
+          previewModel =  Instantiate(groundTilePrefabs[_curTilePrefabIndex],transform,true);
+          previewModel.Preview();
+          //previewModel.GetComponentInChildren<MeshRenderer>().materials[0] = transparentMat;
+           // previewModel.GetComponentInChildren<BoxCollider>().enabled = false;
+          //previewModel.GetComponent<MeshRenderer>().materials[0].SetColor
+         //previewModel = groundTilePrefabs[_curTilePrefabIndex];
+    }
     
-    public void SetPropIndex(int value) => _curPropPrefabIndex = value;
+    public void SetPropIndex(int value) { 
+        _curPropPrefabIndex = value;
+        if(previewModelObj){
+            Destroy(previewModelObj.gameObject);
+        }
+        previewModelObj = (GameObject) Instantiate(prop[_curPropPrefabIndex]);
+        Collider[] colliders = previewModelObj.GetComponentsInChildren<Collider>();
+        MeshRenderer[] renderers = previewModelObj.GetComponentsInChildren<MeshRenderer>();
+        foreach (Collider collider in colliders)
+        {
+            collider.enabled = false;
+        }
+        foreach (MeshRenderer renderer in renderers)
+        {
+            renderer.sharedMaterial = transparentMat;
+        }
+        //previewModel.transform.parent = null;
+       // previewModelObj.GetComponent<MeshRenderer>().sharedMaterial = transparentMat;
+    }
     
-
     public void SaveLevel()
     {
         pointer.gameObject.SetActive(false);
@@ -209,6 +283,30 @@ public class LevelDesignTool : MonoBehaviour
         saveSystem.SaveLevel(prefabObject);
     }
 
-    
-   
+    public void ToggleMode(){
+        if (designState == State.Props)
+        {
+            designState = State.Tiling;
+            modeText.text = "Tiling Mode";
+        }
+        else
+        {
+            designState = State.Props;
+            if(previewModel){
+                Destroy(previewModel.gameObject);
+            }
+            modeText.text = "Props Mode";
+        }
+        SetUpMode();
+    }
+    public void SetUpMode(){
+        if(designState == State.Props)
+        {
+            tileWindow.SetActive(false);
+            propWindow.SetActive(true);
+        }else{
+            tileWindow.SetActive(true);
+            propWindow.SetActive(false);
+        }
+    }   
 }
--- Level Builder Tool 3D/Assets/Level-Builder-3D/Scripts/TileRelated/TileInfo.cs
+++ Level Builder Tool 3D/Assets/Level-Builder-3D/Scripts/TileRelated/TileInfo.cs
@@ -4,9 +4,17 @@
     public class TileInfo : MonoBehaviour
     {
         [SerializeField] private GameObject handlers;
+        public Material transparent;
+
+        public GameObject mesh;
 
         public void DisableHandler()
         {
             handlers.SetActive(false);
+        }
+        public void Preview(){
+            print("transparentr");
+            mesh.GetComponent<MeshRenderer>().sharedMaterial = transparent;
+            mesh.GetComponent<BoxCollider>().enabled = false;
         }
     }
